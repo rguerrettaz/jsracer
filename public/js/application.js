15c16311e988892ce402b917e14401e60cde8b77
Ryan Guerrettaz <rguerrettaz@yahoo.com>
2013-03-19 12:59:57 -0700

    ui changes

--- public/js/application.js
+++ public/js/application.js
@@ -2,6 +2,10 @@ $(function(){
   var startTime = null;
   var finishTime;
   var winner;
+  var s1 = $("#player1_strip")
+  var s2 = $("#player2_strip")
+
+
 
   $(document).on("keyup", function(e){
     $('#game-url').hide();
@@ -14,14 +18,22 @@ $(function(){
 
   var $player1_position = $('#player1_strip td.active');
   var $player2_position = $('#player2_strip td.active');
+  
   if (code===49){
         $('#player1_strip td.active').next().addClass('active');
         $('#player1_strip td.active').prev().removeClass('active');
       }
+  else if (code===48){
+        $('#player2_strip td.active').next().addClass('active');
+        $('#player2_strip td.active').prev().removeClass('active');
+      };
+    
     if ($player1_position.is(':last-child')) {
       var winner = $('#player1initials').text();
       var endTime = $.now();
       finishTime = (endTime-startTime);
+        
+
          $.ajax({
           type: "post",
           dataType: "text",
@@ -30,9 +42,9 @@ $(function(){
           url: "/results"
         })
         .done(function(data){
-        $('#player1_strip').hide();
+        s1.hide();
         $('#winner').html("Player 1 won, Player 2 lost");
-        $('#player2_strip').hide();
+        s2.hide();
         $('td').removeClass('active');
         $('#results').show();
         console.log(data);
@@ -41,13 +53,15 @@ $(function(){
         .fail(function(jqXHR, textStatus, errorThrown){
           alert(textStatus);
         });
-        
-        return false;
-      }
-      else if ($player2_position.is(':last-child')) {
+      };
+
+
+        if ($player2_position.is(':last-child')) {
         var winner = $('#player2initials').text();
         var endTime = $.now();
         finishTime = (endTime-startTime);
+        
+
          $.ajax({
           type: "post",
           dataType: "text",
@@ -56,32 +70,18 @@ $(function(){
           url: "/results"
         })
          .done(function(data){
-        $('#player1_strip').hide();
-        $('#winner').html("Player 1 won, Player 2 lost");
+        s1.hide();
+        $('#winner').html("Player 2 won, Player 1 lost");
         $('#player2_strip').hide();
         $('td').removeClass('active');
         $('#results').show();
         $('#game-url').show();
       })
-        $('#player1_strip').hide();
-        $('#winner').append("Player 2 won, Player 1 lost");
-        $('#player2_strip').hide();
-        $('td').removeClass('active');
-        $('#results').show();
-        return false;
-      }
-      else if(e.keyCode===49){
-        console.log("ASDFASDFSAF");
-        $('#player1_strip td.active').next().addClass('active');
-        $('#player1_strip td.active').prev().removeClass('active');
-      }
-      else if (e.keyCode===48){
-        console.log("ASDFASDFSAF");
-        $('#player2_strip td.active').next().addClass('active');
-        $('#player2_strip td.active').prev().removeClass('active');
-      
-        console.log("did you get here");
+        .fail(function(jqXHR, textStatus, errorThrown){
+          alert(textStatus);
+        });
       };
+      
     });
     $('#reset').click(function(e){
       e.preventDefault();
@@ -97,8 +97,8 @@ $(function(){
       });
       $('#game-url').hide();
       $('#winner').empty();
-      $("#player1_strip").show();
-      $("#player2_strip").show();
+      s1.show();
+      s2.show();
       $('#player1_strip td:first-child').addClass('active');
       $('#player2_strip td:first-child').addClass('active');
     });
